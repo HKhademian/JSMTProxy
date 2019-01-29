7058acf29164cbcc4048c4a87d8b73197cccca36
Hossain Khademian <hco@post.com>
2019-01-29 10:23:38 +0330

    heroku

---
+++ test.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+})
+
+server.listen(PORT, (err) => {
+  if (err) return console.log('something bad happened', err)
+
+  console.log(`server is listening on ${PORT}`)
+})
